1a8ef7ce3f37d057b3afb1aad60b55eb830cbc4b
HarriHiie <89829583+HarriHiie@users.noreply.github.com>
2023-02-13 14:44:48 +0200

    Add files via upload

---
+++ foreach6.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var domains = new Dictionary<string, string>
+            {
+                
+                {"est", "estonia"},
+                {"lat", "latvia" },
+                {"fin", "finland" },
+                {"swe", "sweden" },
+
+            };
+            int i = 1;
+            foreach (var pair in domains)
+            {
+                Console.WriteLine($"{pair.Value} - {pair.Key} - {i}");
+                i++;
+            }
+        }
+    }
+}
